720ed868d7c798e22c3016bf0b264ec43baa95f5
Roberto <robertosegurax17@gmail.com>
2023-01-05 13:13:47 -0800

    changed get route and added scipt tag for heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 ;
 const app = express();
 const server = new ApolloServer({
 	typeDefs,
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/'));
+	res.sendFile(path.join(__dirname, '../client'));
 })
 
 // Create a new instance of an Apollo server with the GraphQL schema
